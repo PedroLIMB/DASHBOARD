7658065e8a59c1cc0602c9a899d4f2b76c138316
PedroLIMB <githublimb@gmail.com>
2023-08-08 13:02:39 -0300

    V1.1

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// Dados fictícios para o gráfico de linha
 const data = {
     labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
     datasets: [{
         label: 'Vendas',
-        data: [5000, 7000, 4000, 8000, 6000, 9000], // Valores fictícios, substitua pelos seus dados reais
+        data: [5000, 7000, 4000, 8000, 6000, 9000],
         borderColor: '#007BFF',
         backgroundColor: 'rgba(0, 123, 255, 0.2)',
         borderWidth: 2
     }]
 };
 
-// Configuração do gráfico
 const config = {
     type: 'line',
     data: data,
@@ -25,6 +23,5 @@ const config = {
     }
 };
 
-// Criando o gráfico
 const ctx = document.getElementById('sales-chart').getContext('2d');
 new Chart(ctx, config);
